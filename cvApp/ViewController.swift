48283d3ccfb68e8aa8f61bc34ac2b15a4304ef0e
Oliver Köping <kool18dw@mace2432-20.jth-stud2.hj.se>
2019-10-31 15:05:16 +0100

    init commit

---
+++ cvApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cvApp
+//
+//  Created by Oliver Köping on 2019-10-31.
+//  Copyright © 2019 Oliver Köping. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
