0db3e1e282a3c5c13128f968f4e9469869910710
Oliver Köping <kool18dw@mace2432-17.jth-stud2.hj.se>
2019-11-05 11:29:46 +0100

    First viewcontroller done

---
+++ cvApp/ExperienceViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ExperienceViewController.swift
+//  cvApp
+//
+//  Created by Oliver Köping on 2019-11-05.
+//  Copyright © 2019 Oliver Köping. All rights reserved.
+//
+
+import UIKit
+
+class ExperienceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
